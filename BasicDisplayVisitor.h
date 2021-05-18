6a0fd28f404dc22b87797a17d0e717631ccc655d
Irtaza Waseem <54458381+irtaza210@users.noreply.github.com>
2021-05-17 22:42:59 -0500

    Update BasicDisplayVisitor.h

--- BasicDisplayVisitor.h
+++ BasicDisplayVisitor.h
@@ -1,7 +1,5 @@
 /*
 
-Authors: Aaron Stein, Rain Chen, Irtaza Waseem
-CSE332 Lab 5
 
 This file declares the necessary functions that are used to "visit" different types of files
 and display their contents as part of the visitor pattern.
