4e1217dc183b3855c7c48f928a50255a28505f3a
Irtaza Waseem <54458381+irtaza210@users.noreply.github.com>
2021-05-17 22:48:16 -0500

    Update SimpleFileFactory.h

--- SimpleFileFactory.h
+++ SimpleFileFactory.h
@@ -1,7 +1,6 @@
 /*
 
-Authors: Aaron Stein, Rain Chen, Irtaza Waseem
-CSE332 Lab 5
+
 
 This file declares the necessary functions for the SimpleFileFactory, which is a particular
 implementation of the AbstractFileFactory interface.
