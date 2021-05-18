06930174156e42c9d1c684c922839e36789cd38c
Irtaza Waseem <54458381+irtaza210@users.noreply.github.com>
2021-05-17 22:45:06 -0500

    Update DisplayCommand.cpp

--- DisplayCommand.cpp
+++ DisplayCommand.cpp
@@ -1,7 +1,5 @@
 /*
 
-Authors: Aaron Stein, Rain Chen, Irtaza Waseem
-CSE332 Lab 5
 
 This file defines the necessary methods for the display command, which can display
 files either as their data, or as formatted data for img files.
